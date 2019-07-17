b3e5374ad92ae37abb96cb6d19fde810ec5c6087
Zachary M Waxman <zwaxman@gmail.com>
2019-07-17 00:47:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var newArr=[];
+  for(var i=0;i<musicians.length;i++){
+    newArr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newArr
+}
